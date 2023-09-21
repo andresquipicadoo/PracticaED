c89b7190adb3ae6bc6d54fe735bb1478995e08c7
Jeffry Araya Ch <jeffry2811ach@gmail.com>
2023-09-21 10:42:09 -0600

    documentacion finalizada

--- Practica/main.cpp
+++ Practica/main.cpp
@@ -1,3 +1,31 @@
 //
 // Created by aquir on 20/9/2023.
 //
+
+
+#include <iostream>
+#include <stack>
+#include "pila.cpp"
+
+using namespace std;
+
+int main() {
+    // Punto A: Piezas de Lego de diferentes tipos
+    piezalego pieza1 = {"Pared", 10, 5, 2};
+    piezalego pieza2 = {"Ventana", 5, 5, 2};
+    piezalego pieza3 = {"Techo", 10, 10, 2};
+    piezalego pieza4 = {"Piso", 10, 10, 2};
+
+    Hogar casa;
+
+    casa.construirPared(); // Construir una pared
+    casa.construirPared(); // Construir otra pared
+    casa.construirVentana(); // Construir una ventana
+    casa.construirTecho(); // Construir el techo
+    casa.construirPiso(); // Construir el piso
+
+    // Mostrar la casa construida
+    casa.mostrarCasa();
+
+    return 0;
+}
--- Practica/pila.cpp
+++ Practica/pila.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 // Definimos una estructura para representar las piezas de Lego
+/**
+ * @struct piezalego
+ * @brief A struct representing a lego piece with its type, length, width and height.
+ */
 struct piezalego {
     string type;
     int largo;
@@ -73,58 +77,3 @@ class Hogar {
             cout << "Piso: " << piso.size() << endl;
         }
 };
-
-/**
- * @brief Función para evaluar expresiones matemáticas utilizando pilas.
- * 
- * @param expresion Cadena de caracteres que representa la expresión a evaluar.
- * @return int Resultado de la evaluación de la expresión.
- */
-int evaluarExpresion(const string& expresion) {
-    stack<int> pila;
-    for (char c: expresion) {
-        if (isdigit(c)) {
-            int numero = c - '0';
-            pila.push(numero);
-        } else if (c == '+') {
-            int operand2 = pila.top();
-            pila.pop();
-            int operand1 = pila.top();
-            pila.pop();
-            pila.push(operand1 + operand2);
-        } else if (c == '-') {
-            int operand2 = pila.top();
-            pila.pop();
-            int operand1 = pila.top();
-            pila.pop();
-            pila.push(operand1 - operand2);
-        }
-    }
-    return pila.top();
-}
-
-int main() {
-    // Punto A: Piezas de Lego de diferentes tipos
-    piezalego pieza1 = {"Pared"};
-    piezalego pieza2 = {"Ventana"};
-    piezalego pieza3 = {"Techo"};
-    piezalego pieza4 = {"Piso"};
-
-    Hogar casa;
-
-    casa.construirPared(); // Construir una pared
-    casa.construirPared(); // Construir otra pared
-    casa.construirVentana(); // Construir una ventana
-    casa.construirTecho(); // Construir el techo
-    casa.construirPiso(); // Construir el piso
-
-    // Mostrar la casa construida
-    casa.mostrarCasa();
-
-    // Punto C: Evaluación de expresiones con pilas
-    string expresion = "2+3-1";
-    int resultado = evaluarExpresion(expresion);
-    cout << "Resultado de la expresión: " << resultado << endl;
-
-    return 0;
-}
